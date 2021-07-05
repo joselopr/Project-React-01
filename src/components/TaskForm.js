65cb48a61696292a9c60b3061c28925df8929619
jose prieto <josel0priet0@gmail.com>
2021-07-05 18:09:21 +0200

    need addTask function added

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
 
+  state = {
+    title: '',
+    description: ''
+  }
+
   onSubmit = e => {
+    this.props.addTask(this.state.title, this.state.description)
     e.preventDefault();
   }
 
   onChange = e => {
-    console.log(e.target.value)
+    this.setState({
+      title: e.target.value,
+      description: e.target.value
+    })
   }
 
   render() {
+
     return (
       <form onSubmit={this.onSubmit}>
-        <input type="text" placeholder='Write a Task' onChange={this.onChange} />
+        <input
+        type="text"
+        name="title"
+        placeholder='Write a Task' onChange={this.onChange} value={this.state.title} />
           <br />
           <br />
-        <textarea placeholder='Write a description' onChange={this.onChange}> </textarea>
+        <textarea placeholder='Write a description' onChange={this.onChange} value={this.state.description}> </textarea>
         <input type="submit" />
       </form>
       )
