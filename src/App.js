69b67d8c8a8f80e9caae4dd1a489a1eca929e7b7
jose prieto <josel0priet0@gmail.com>
2021-07-05 12:29:13 +0200

    some states and props added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// function Helloworld(props) {
+//   return (
+//     <div id='hello'>
+//     <h3> {props.subtitle} </h3>
+//     {props.mytext}
+//     </div>
+//   );
+// }
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+    return (
+      <div id='hello'>
+        <h3> {this.props.subtitle} </h3>
+        {this.props.mytext}
+        <button onClick={this.toggleShow}> Toggle Show</button>
+      </div>
+    );
+
+    } else {
+      return <h1>
+      There is no elements
+      <button onClick={this.toggleShow}>Toggle Show</button>
+      </h1>
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>This is my component:
+      <Helloworld mytext='Hello Fatz' subtitle='loremp ispum'/>
+      <Helloworld mytext='Hola mundo entero' subtitle='Todo bien'/>
+      <Helloworld mytext='Bonjour World' subtitle='Ce va tres bien' />
     </div>
   );
 }
